320b95bea72f308320869269e0c2653e3e54b494
dajin <djinii@dajinii-MacBookPro.local>
2024-02-23 15:17:27 +0900

    new

---
+++ src/main/java/com/dutchOrder/server/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.dutchOrder.server.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	// vue.js랑 연결관련 클래스
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 허용할 URL 패턴
+                .allowedOrigins("http://localhost:8089") // 클라이언트(Vue) 포트
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowCredentials(true); // 인증 정보 허용
+    }
+}
